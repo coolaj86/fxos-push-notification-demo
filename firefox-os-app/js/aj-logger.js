2a60450177c54412c6ea5b241fe40977f9f609c0
AJ ONeal <awesome@coolaj86.com>
2014-07-21 22:34:42 -0600

    register with home base

---
+++ firefox-os-app/js/aj-logger.js
@@ -0,0 +1,35 @@
+window.AjLogger = {
+  create: function (id) {
+    function log(msg, type) {
+      if (/^info/.test(type) || !type) {
+        type = 'info';
+      }
+      else if (/^suc/.test(type)) {
+        type = 'success';
+      }
+      else if (/^warn/.test(type)) {
+        type = 'warning';
+      }
+      else if (/^err/.test(type) || /^dang/.test(type)) {
+        type = 'danger';
+        msg = msg && msg.message || msg;
+      }
+
+      $(id).append('<div class="alert alert-' + type + '">' + msg + '</div>');
+    }
+    log.info = function (msg) {
+      log(msg, 'success');
+    };
+    log.warn = function (msg) {
+      log(msg, 'warn');
+    };
+    log.error = function (msg) {
+      log(msg, 'error');
+    };
+    log.clear = function () {
+      $(id).html('');
+    };
+
+    return log;
+  }
+};
--- firefox-os-app/js/app.js
+++ firefox-os-app/js/app.js
@@ -1,126 +1,81 @@
+//
+// A lovely Simple Push Notification demo
+//
+// https://developer.mozilla.org/en-US/docs/WebAPI
+// https://developer.mozilla.org/en-US/docs/Web/API/Simple_Push_API
+// https://developer.mozilla.org/en-US/Apps/Build/App_permissions
+// https://developer.mozilla.org/en-US/Apps/Build/API_support_table
+
 $(function() {
   'use strict';
 
-  function log(msg, type) {
-    if (/^info/.test(type) || !type) {
-      type = 'info';
-    }
-    else if (/^warn/.test(type)) {
-      type = 'warning';
-    }
-    else if (/^err/.test(type) || /^dang/.test(type)) {
-      type = 'danger';
-    }
+  window.onerror = function (e) {
+    console.error('[window.onerror] uncaught exception');
+    console.error(e);
+    log.error('[window.onerror] uncaught exception');
+    log.error(e && e.message || e);
+  };
 
-    $('#console').append('<div class="alert alert-' + type + '">' + msg + '</div>');
-  }
+  // using systemXHR
+  $.ajaxSetup( {
+    xhr: function() { return new window.XMLHttpRequest({ mozSystem: true, mozAnon: true }); }
+  });
 
   // TODO Promise
-  var db = new window.PouchDB('settings')
-    , pushRegistration
-    , homeBase = 'http://ffpush.dev.coolaj86.com'
+  var log = window.AjLogger.create('#console')
+    , db = new window.PouchDB('settings')
+    , homeBase = 'http://ffpush.dev.coolaj86.com:8080'
     , home = homeBase + '/api/push'
     //, home = 'https://u34hasta3bs5.runscope.net'
     ;
 
-  // https://developer.mozilla.org/en-US/docs/WebAPI
-  // https://developer.mozilla.org/en-US/docs/Web/API/Simple_Push_API
-  // https://developer.mozilla.org/en-US/Apps/Build/App_permissions
-  // https://developer.mozilla.org/en-US/Apps/Build/API_support_table
-
-  db.get('reg_url', function (err, doc) {
-    pushRegistration = doc || { _id: 'reg_url', url: '' };
-  });
-
   $('#push-form').on('submit', function (ev) {
-    //ev.defaultPrevented = true;
     ev.preventDefault();
     ev.stopPropagation();
 
-    pushRegistration.url = $('#reg-url').val();
-    db.put(pushRegistration);
     db.get('push_endpoint', function (err, doc) {
-      /*
       if (err && 404 !== err.status) {
         console.error(err);
-        window.alert('Error with PouchDB / IndexedDB: ' + err.message);
+        log.alert('Error with PouchDB / IndexedDB: ' + err.message);
       }
-      */
 
       if (!doc) {
         registerMozilla();
       } else if (!doc.friendlyId) {
-        registerHome(doc);
+        registerHome(doc.url, doc.oldUrl);
       } else {
-        log('Already Registered');
-        log(doc.friendlyId);
-        log(doc.url);
+        showRegistration(doc);
       }
     });
   });
 
   $('body').on('click', '#console-clear', function () {
-    $('#console').html('');
+    log.clear();
   });
 
-  function registerHome(endpoint) {
-    $.post(home, { endpoint: endpoint }).then(function (data) {
-      log('communicated with server');
-      log(JSON.stringify(data));
-
-      db.get('push_endpoint', function (err, data) {
-        if (!data || !data.url) {
-          log.error('db data disappeared', 'error');
-          data = { _id: 'push_endpoint' };
-        }
-
-        data.url = endpoint;
-        data.friendlyId = data.id;
-
-        db.put(data, function (err) {
-          if (err) {
-            log('error storing in pouch', 'error');
-            log(err && err.message || err, 'error');
-          }
-        });
-      }, function (err) {
-        log('error communicating with server', 'error');
-        log(err && err.message || err, 'error');
-      });
-    });
-  }
-
-  function registerMozilla() {
+  // Register with Mozilla for a push notification
+  function registerMozilla(oldUrl) {
     if (!window.navigator.push) {
       console.error('missing navigator.push');
     }
  
-    console.log('registering');
     var req = navigator.push.register()
       ;
     
     log('Registering for push notification...');
-    req.addEventListener('success', function (ev) {
-      log(req.result, 'info');
-
+    req.addEventListener('success', function (/*ev*/) {
       var endpoint = req.result
-        //, request = new XMLHttpRequest({ mozSystem: true, mozAnon: true })
         ;
-
-      console.log("New endpoint: " + endpoint);
-      $('#response').text("New endpoint: " + endpoint);
-      console.log(ev);
-      registerHome(endpoint);
-
-      /*
-      request.open(pushRegistration.url, "POST", true);
-      request.setHeader('Content-Type', 'application/json');
-      request.send(JSON.stringify({ url: endpoint }, null, '  '));
-      */
 
       db.put({
         _id: 'push_endpoint'
       , url: req.result
+      }, function (err) {
+        if (err) {
+          log.error('error saving endpoint to Pouch');
+          log.error(err);
+        }
+        registerHome(endpoint, oldUrl);
       });
     });
 
@@ -129,20 +84,72 @@ $(function() {
       console.error("Error getting a new endpoint: " + req.error);
       console.error("Error getting a new endpoint: " + req.error.name);
 
-      log(ev.target.error.name, 'error');
-      log(req.error, 'error');
-      log(req.error.name, 'error');
+      log.error(ev.target.error.name);
+      log.error(req.error);
+      log.error(req.error.name);
     });
   }
 
-  if (window.navigator.mozSetMessageHandler) {
+  // Register at our own server to use that url
+  function registerHome(endpoint, oldUrl) {
+    // TODO update server to keep friendlyId the same
+    $.post(home, { url: endpoint, previous: oldUrl }).then(
+      function (body) {
+        db.get('push_endpoint', function (err, doc) {
+          if (!doc || !doc.url) {
+            log.error('db data disappeared');
+            doc = { _id: 'push_endpoint' };
+          }
+
+          doc.url = endpoint;
+          doc.friendlyId = body.id;
+
+          db.put(doc, function (err) {
+            if (err) {
+              log('error storing in pouch', 'error');
+              log(err && err.message || err, 'error');
+            } else {
+              showRegistration(doc);
+            }
+          });
+        });
+      }
+    , function (err) {
+        // NOTE: you may never get here because Firefox OS
+        // doesn't send you data from non-200 requests
+        log('error communicating with server', 'error');
+        log(err && err.message || err, 'error');
+      });
+  }
+
+  function showRegistration(doc) {
+    log.clear();
+    log('Registered');
+    log.info(doc.friendlyId);
+    log(doc.url);
+  }
+
+
+  // Receive the push notifications
+  if (!window.navigator.mozSetMessageHandler) {
     window.navigator.mozSetMessageHandler('push', function(ev) {
-      log("'push' received " + ev.version + " " + ev.pushEndpoint);
-      console.log('My endpoint is ' + ev.pushEndpoint);
-      console.log('My new version is ' +  ev.version);
-      //Remember that you can handle here if you have more than
-      //one pushEndpoint
-      // doStuff()
+      log('[push notification] v' + ev.version);
+      log('from ' + ev.pushEndpoint);
+      log("retrieving... ");
+      
+      db.get('push_endpoint', function (err, doc) {
+        $.get(home + '/' + doc.friendlyId + '/data')
+          .then(
+            function () {
+              log.clear();
+              log('[push notification] retrieved data:');
+              log(JSON.stringify(doc));
+            }
+          , function () {
+              log.error("failed to retrieve push data");
+            }
+          );
+      });
     });
   } else {
     // No message handler
@@ -150,8 +157,7 @@ $(function() {
 
   if (window.navigator.mozSetMessageHandler) {
     window.navigator.mozSetMessageHandler('push-register', function() {
-      log("'push-register' received.", 'warn');
-      log("The old endpoint is stale and will now be renewed");
+      log.warn("[push-register] renewing endpoint...");
       
       db.get('push_endpoint', function(err, doc) {
         if (!doc) {
@@ -159,9 +165,8 @@ $(function() {
           return;
         }
 
-        // db.remove(doc, function(err, response) { });
         db.remove(doc._id, doc._rev, function (/*err, response*/) {
-          registerMozilla();
+          registerMozilla(doc.url);
         });
       });
     });
